3346bc0c6f441afdbfd6e8a12e340868b22e2384
eleung32 <52718704+eleung32@users.noreply.github.com>
2020-12-08 14:28:51 -0500

    added console.log

--- recurring.js
+++ recurring.js
@@ -1,4 +1,4 @@
-///Google Question
+///Google Question First Recurring Character
 //Given an array = [2,5,1,2,3,5,1,2,4]:
 //It should return 2
 
@@ -8,7 +8,8 @@
 //Given an array = [2,3,4,5]:
 //It should return undefined
 
-//naive approach
+//naive method 
+//time O(n^2)
 function firstRecurringCharacter(input){
     for (let i=0; i<input.length; i++){
         for (let j=i+1; j<input.length; j++){
@@ -20,4 +21,24 @@ function firstRecurringCharacter(input){
     return undefined;
 }
 
-alert(firstRecurringCharacter([2,5,5,2,3,5,1,2,4]))
+alert(firstRecurringCharacter([2,1,1,2,3,5,1,2,4]))
+
+
+//hash table objects method
+//time O(n)
+//memory O(n)
+function firstRecurringCharacter2(input) {
+    let map = {};
+    for (let i=0; i<input.length; i++) {
+         //console.log(map[input[i]]);
+        if (map[input[i]] !== undefined){
+            return input[i]
+        } else {
+            map[input[i]] = i;
+        }
+        // console.log(map)
+    }
+    return undefined
+}
+
+alert(firstRecurringCharacter2([2,1,1,2,3,5,1,2,4]))
