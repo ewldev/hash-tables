130eeecf98d35b4cf45be147300ce1d859ef8235
eleung32 <52718704+eleung32@users.noreply.github.com>
2020-12-10 23:56:48 -0500

    implement hash table

--- recurring.js
+++ recurring.js
@@ -10,6 +10,7 @@
 
 //naive method 
 //time O(n^2)
+//space O(1)
 function firstRecurringCharacter(input){
     for (let i=0; i<input.length; i++){
         for (let j=i+1; j<input.length; j++){
@@ -21,24 +22,24 @@ function firstRecurringCharacter(input){
     return undefined;
 }
 
-alert(firstRecurringCharacter([2,1,1,2,3,5,1,2,4]))
+//alert(firstRecurringCharacter([2,5,5,2,3,5,1,2,4]))
 
 
 //hash table objects method
 //time O(n)
-//memory O(n)
+//space O(n)
 function firstRecurringCharacter2(input) {
     let map = {};
     for (let i=0; i<input.length; i++) {
-         //console.log(map[input[i]]);
+         console.log(map[input[i]]);
         if (map[input[i]] !== undefined){
             return input[i]
         } else {
-            map[input[i]] = i;
+           map[input[i]] = i;
         }
         // console.log(map)
     }
     return undefined
 }
 
-alert(firstRecurringCharacter2([2,1,1,2,3,5,1,2,4]))
+alert(firstRecurringCharacter2([2,5,5,2,3,5,1,2,4]))
